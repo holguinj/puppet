a857727c4563ab0fcdaa87ecd90426160d79329f
Justin Holguin <justin.h.holguin@gmail.com>
2017-08-11 15:26:47 -0700

    rebase this over dan

--- lib/puppet/info_service/task_information_service.rb
+++ lib/puppet/info_service/task_information_service.rb
@@ -2,7 +2,7 @@ class Puppet::InfoService::TaskInformationService
 
   def self.tasks_per_environment(environment_name)
     # get the actual environment object, raise error if the named env doesn't exist
-    env = Puppet.lookup(:environments).get(environment_name)
+    env = Puppet.lookup(:environments).get!(environment_name)
     return [] unless env
 
     env.modules.map do |mod|
@@ -13,15 +13,14 @@ class Puppet::InfoService::TaskInformationService
   end
 
   def self.task_data(environment_name, module_name, task_name)
-    empty_task = {:metadata_file => nil, :files => nil}
-    return empty_task unless Puppet.lookup(:environments).get(environment_name)
+    # raise EnvironmentNotFound if applicable
+    Puppet.lookup(:environments).get!(environment_name)
 
-    # will throw if the environment doesn't exist, hence the check above
     pup_module = Puppet::Module.find(module_name, environment_name)
-    return empty_task unless pup_module
+    raise Puppet::Module::MissingModule unless pup_module
 
     task = pup_module.tasks.find { |t| t.name == task_name }
-    return empty_task unless task
+    raise Puppet::Module::Task::TaskNotFound unless task
 
     {:metadata_file => task.metadata_file,
      :files => task.files}
